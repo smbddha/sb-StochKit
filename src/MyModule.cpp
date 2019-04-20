be0b10a71bcfa76de6c0b3f563b0faf1d8c60fd8
smbddha <samuelwlaing@gmail.com>
2019-04-20 14:33:05 -0400

    changes for new panel + new knob / control for env changing

--- src/MyModule.cpp
+++ src/MyModule.cpp
@@ -6,6 +6,7 @@ struct MyModule : Module {
 	enum ParamIds {
 		FREQ_PARAM,
     STEP_PARAM,
+    DSTP_PARAM,
     BPTS_PARAM,
     GRAT_PARAM,
     TRIG_PARAM,
@@ -13,8 +14,13 @@ struct MyModule : Module {
     NUM_PARAMS
 	};
 	enum InputIds {
-		WAV0_INPUT,
-		NUM_INPUTS
+		FREQ_INPUT,
+    STEP_INPUT,
+    DSTP_INPUT,
+    BPTS_INPUT,
+    GRAT_INPUT,
+    ENVS_INPUT,
+    NUM_INPUTS
 	};
 	enum OutputIds {
 		SINE_OUTPUT,
@@ -62,20 +68,26 @@ void MyModule::step() {
   int new_nbpts = clamp((int) params[BPTS_PARAM].value, 3, MAX_BPTS);
   if (new_nbpts != go.num_bpts) go.num_bpts = new_nbpts;
 
-  int env_next = (int) params[ENVS_PARAM].value + 1;
-  if (env != (EnvType) env_next) {
-    debug("Switching to env type: %d", env_next);
-    env = (EnvType) env_next;
+  //int env_next = (int) params[ENVS_PARAM].value + 1;
+  
+  // snap knob for selecting envelope for the grain
+  int env_num = (int) clamp(roundf(params[ENVS_PARAM].value), 1.0f, 8.0f);
+
+  if (env != (EnvType) env_num) {
+    debug("Switching to env type: %d", env_num);
+    env = (EnvType) env_num;
     go.env.switchEnvType(env);
   }
 
   //if (phase >= 1.0) debug("PITCH PARAM: %f\n", (float) params[PITCH_PARAM].value);
-
-  if (smpTrigger.process(params[TRIG_PARAM].value)) {
-    
-  }
-  
+  //TODO
+  // add mirroring function to complement the wrap
+  // and then add switch to change between the two
+ 
+  // TODO
+  // accept modulation of signal inputs for each parameter
   go.max_amp_step = rescale(params[STEP_PARAM].value, 0.0, 1.0, 0.05, 0.3);
+  go.max_dur_step = rescale(params[DSTP_PARAM].value, 0.0, 1.0, 0.01, 0.3);
   go.freq_mul = rescale(params[FREQ_PARAM].value, -1.0, 1.0, 0.05, 4.0);
   go.g_rate = params[GRAT_PARAM].value * 5.f;
 
@@ -95,18 +107,31 @@ struct MyModuleWidget : ModuleWidget {
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 6 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-    addParam(ParamWidget::create<CKSSThree>(Vec(110, 240), module, MyModule::ENVS_PARAM, 0.0f, 2.0f, 0.0f));
+    //addParam(ParamWidget::create<CKSSThree>(Vec(110, 240), module, MyModule::ENVS_PARAM, 0.0f, 2.0f, 0.0f));
 		
-    addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(40, 140), module, MyModule::FREQ_PARAM, -1.0, 1.0, 0.0));
-    addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(110, 140), module, MyModule::STEP_PARAM, 0.0, 1.0, 0.9));
-    addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(40, 200), module, MyModule::GRAT_PARAM, 0.7, 1.3, 0.0));
-    addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(110, 200), module, MyModule::BPTS_PARAM, 3, MAX_BPTS, 0));
-    addParam(ParamWidget::create<CKD6>(Vec(40, 70), module, MyModule::TRIG_PARAM, 0.0f, 1.0f, 0.0f));
-		
-    addInput(Port::create<PJ301MPort>(Vec(33, 245), Port::INPUT, module, MyModule::WAV0_INPUT));
-		addOutput(Port::create<PJ301MPort>(Vec(33, 275), Port::OUTPUT, module, MyModule::SINE_OUTPUT));
+    addParam(ParamWidget::create<RoundHugeBlackKnob>(Vec(15, 68), module, MyModule::FREQ_PARAM, -1.0, 1.0, 0.0));
+    addParam(ParamWidget::create<RoundLargeBlackKnob>(Vec(113.328, 62.95), module, MyModule::BPTS_PARAM, 3, MAX_BPTS, 0));
+    
+    addParam(ParamWidget::create<RoundLargeBlackKnob>(Vec(14, 177.16), module, MyModule::DSTP_PARAM, 0.0, 1.0, 0.9));
+    addParam(ParamWidget::create<RoundLargeBlackKnob>(Vec(92, 177.16), module, MyModule::STEP_PARAM, 0.0, 1.0, 0.9));
+    
+    addParam(ParamWidget::create<RoundLargeBlackKnob>(Vec(124.781, 275.91), module, MyModule::GRAT_PARAM, 0.7, 1.3, 0.0));
+    
+    addParam(ParamWidget::create<RoundBlackSnapKnob>(Vec(12, 275.83), module, MyModule::ENVS_PARAM, 1.0f, 4.0f, 4.0f));
+	
+    // signal inputs
+    addInput(Port::create<PJ301MPort>(Vec(79.022, 133.72), Port::INPUT, module, MyModule::FREQ_INPUT));
+    addInput(Port::create<PJ301MPort>(Vec(121.022, 133.72), Port::INPUT, module, MyModule::BPTS_INPUT));
+    
+    addInput(Port::create<PJ301MPort>(Vec(59.187, 224.25), Port::INPUT, module, MyModule::STEP_INPUT));
+		addInput(Port::create<PJ301MPort>(Vec(137.187, 224.25), Port::INPUT, module, MyModule::DSTP_INPUT));
+    
+    addInput(Port::create<PJ301MPort>(Vec(19.875, 340.42), Port::INPUT, module, MyModule::ENVS_INPUT));
+		addInput(Port::create<PJ301MPort>(Vec(76.210, 285.33), Port::INPUT, module, MyModule::GRAT_INPUT));
 
-		addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, MyModule::BLINK_LIGHT));
+    addOutput(Port::create<PJ301MPort>(Vec(134.003, 334.86), Port::OUTPUT, module, MyModule::SINE_OUTPUT));
+
+		//addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, MyModule::BLINK_LIGHT));
 	}
 };
 
