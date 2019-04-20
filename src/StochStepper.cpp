8e269576c1e268404027a0ac64325fdeee7d7403
smbddha <samuelwlaing@gmail.com>
2019-04-20 17:11:00 -0400

    stitcher stutter controls ; moved stoch stepper module to new file + boilerplate

---
+++ src/StochStepper.cpp
@@ -0,0 +1,91 @@
+#include "Gendy.hpp"
+
+struct StochStepper : Module {
+	enum ParamIds {
+    NUM_PARAMS
+	};
+	enum InputIds {
+    NUM_INPUTS
+	};
+	enum OutputIds {
+		STOC_OUTPUT,
+    NUM_OUTPUTS
+	};
+	enum LightIds {
+		NUM_LIGHTS
+	};
+
+	//float phase = 1.0;
+	float blinkPhase = 0.0;
+
+  enum StepTypes {
+    RAMP,
+    LINEAR,
+    EXP,
+    CUBIC
+  };
+
+  SchmittTrigger stepTrigger;
+  GendyOscillator go;
+
+  float amp = 0.f;
+  float amp_next = 0.f;
+  float amp_out = 0.f;
+
+  StochStepper() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
+  }
+	
+  void step() override;
+  float wrap(float,float,float);
+};
+
+void StochStepper::step() {
+	// Implement a simple sine oscillator
+  float deltaTime = engineGetSampleTime();
+  
+  if (stepTrigger.process(inputs[STEP_INPUT].value / 2.f)) {
+    switch (t_step) {
+      case RAMP:
+        break;
+      case LINEAR:
+        break;
+      case EXP:
+        break;
+      case CUBIC:
+        break;
+      default:
+        break;
+    } 
+  }
+ 
+  // continue with interpolation if not doing ramp
+
+  outputs[STOC_OUTPUT].value = 5.0f;
+}
+
+struct StochStepperWidget : ModuleWidget {
+	StochStepperWidget(StochStepper *module) : ModuleWidget(module) {
+		setPanel(SVG::load(assetPlugin(plugin, "res/StochStepper.svg")));
+
+		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 6 * RACK_GRID_WIDTH, 0)));
+		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 6 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+    //addParam(ParamWidget::create<CKSSThree>(Vec(110, 240), module, StochStepper::ENVS_PARAM, 0.0f, 2.0f, 0.0f));
+		
+    //addParam(ParamWidget::create<RoundHugeBlackKnob>(Vec(15, 68), module, StochStepper::FREQ_PARAM, -1.0, 1.0, 0.0));
+   	//addInput(Port::create<PJ301MPort>(Vec(76.210, 285.33), Port::INPUT, module, StochStepper::GRAT_INPUT));
+
+    addOutput(Port::create<PJ301MPort>(Vec(134.003, 334.86), Port::OUTPUT, module, StochStepper::STOC_OUTPUT));
+
+		//addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, StochStepper::BLINK_LIGHT));
+	}
+};
+
+
+// Specify the Module and ModuleWidget subclass, human-readable
+// author name for categorization per plugin, module slug (should never
+// change), human-readable module name, and any number of tags
+// (found in `include/tags.hpp`) separated by commas.
+Model *modelStochStepper = Model::create<StochStepper, StochStepperWidget>("Gendy", "StochStepper", "Stochastic Stepper", OSCILLATOR_TAG);
