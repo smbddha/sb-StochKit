1959ecafafe9366696c95b0d8a6892b707368a5a
smbddha <samuelwlaing@gmail.com>
2019-04-20 21:45:58 -0400

    work on stepper

--- src/StochStepper.cpp
+++ src/StochStepper.cpp
@@ -3,6 +3,39 @@
 #include "util/common.hpp"
 #include "dsp/digital.hpp"
 
+enum DistributionEqs {
+  UNIFORM,
+  EXPONENTIAL,
+  POISSON,
+  GAUSSIAN,
+  NUM_EQS
+};
+
+/*
+ * gRand
+ * generates a random number based on a 
+ * given probabilty distribution
+ *
+ * @param DistributionEqs type - enum for a specific prop. distribution
+ */
+float gRand(DistributionEqs type) {
+  float n = randomNormal(); 
+  switch (type) {
+    case UNIFORM:
+      break;
+    case EXPONENTIAL:
+      break;
+    case POISSON:
+      break;
+    case GAUSSIAN:
+      break;
+    default:
+      break;
+  }
+
+  return n;
+}
+
 struct StochStepper : Module {
 	enum ParamIds {
     NUM_PARAMS
@@ -25,9 +58,7 @@ struct StochStepper : Module {
   enum StepTypes {
     STEP,
     RAMP,
-    LINEAR,
-    EXP,
-    CUBIC
+    EXP 
   };
 
   SchmittTrigger stepTrigger;
@@ -48,24 +79,26 @@ struct StochStepper : Module {
 void StochStepper::step() {
 	// Implement a simple sine oscillator
   //float deltaTime = engineGetSampleTime();
-  
+ 
+  t_step = (StepTypes) params[STEP_PARAM].value;
+
   if (stepTrigger.process(inputs[STEP_INPUT].value / 2.f)) {
-    switch (t_step) {
-      case STEP:
-        break;
-      case RAMP:
-        break;
-      case LINEAR:
-        break;
-      case EXP:
-        break;
-      case CUBIC:
-        break;
-      default:
-        break;
-    } 
+    // handle the next amplitude generation here using the 
+    // different probability distributions
   }
  
+  switch (t_step) {
+    case STEP:
+      break;
+    case RAMP:
+      break;
+    case LINEAR:
+      break;
+    case EXP:
+      break;
+    default:
+      break;
+  }
   // continue with interpolation if not doing ramp
 
   outputs[STOC_OUTPUT].value = 5.0f;
@@ -80,7 +113,7 @@ struct StochStepperWidget : ModuleWidget {
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 6 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-    //addParam(ParamWidget::create<CKSSThree>(Vec(110, 240), module, StochStepper::ENVS_PARAM, 0.0f, 2.0f, 0.0f));
+    addParam(ParamWidget::create<CKSSThree>(Vec(110, 240), module, StochStepper::STEP_PARAM, 0.0f, 2.0f, 0.0f));
 		
     //addParam(ParamWidget::create<RoundHugeBlackKnob>(Vec(15, 68), module, StochStepper::FREQ_PARAM, -1.0, 1.0, 0.0));
    	//addInput(Port::create<PJ301MPort>(Vec(76.210, 285.33), Port::INPUT, module, StochStepper::GRAT_INPUT));
