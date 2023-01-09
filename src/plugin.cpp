d15e40abf0da60b70218f7ef1f6ae0e918162bc6
Steve Russell <41975381+SteveRussell33@users.noreply.github.com>
2023-01-09 10:53:28 +0000

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -4,8 +4,6 @@ Plugin *pluginInstance;
 
 void init(Plugin *p) {
   pluginInstance = p;
-  p->slug = TOSTRING(SLUG);
-  p->version = TOSTRING(VERSION);
 
   // Add all Models defined throughout the plugin
   p->addModel(modelGenEcho);
